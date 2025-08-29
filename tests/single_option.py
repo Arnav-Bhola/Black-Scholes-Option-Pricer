e98e435ae5c755b0465695a9e7c358d177b5724f
arnav <arnav.bholausa@gmail.com>
2025-08-29 00:30:28 -0400

    Stop tracking tests/ and heatmaps/ folders

--- tests/single_option.py
+++
@@ -1,7 +0,0 @@
-from src.black_scholes import BlackScholes
-from src.enums import OptionType
-
-option = BlackScholes(stock_price=100, strike_price=105, time_to_exp=0.5, risk_free_rate=0.05, volatility=0.2, option_type=OptionType.PUT_OPTION)
-price = option.calculate_price()
-print(option.calculate_greeks())
-print(price)
